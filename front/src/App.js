e86933dfeee1d08e94864e13394c3eec7a4cd6bb
이규훈 <wxyz0904@naver.com>
2023-06-14 17:40:56 +0900

    Feat: 기본페이지 완성
    부트스트랩을 이용한 기본페이지 완성
    백엔드와 연동은 아직 없음

--- front/src/App.js
+++ front/src/App.js
@@ -5,27 +5,81 @@ import Header from "./AlbumHeader";
 const AlbumCard = ({ text }) => {
   return (
     <div className="col">
-      <div className="card shadow-sm">{/* card content */}</div>
-    </div>
-  );
-};
+      <div className="card shadow-sm">
+        <svg
+          className="bd-placeholder-img card-img-top"
+          width="100%"
+          height="225"
+          xmlns="http://www.w3.org/2000/svg"
+          role="img"
+          aria-label="Placeholder: Thumbnail"
+          preserveAspectRatio="xMidYMid slice"
+          focusable="false"
+        >
+          <title>Placeholder</title>
+          <rect width="100%" height="100%" fill="#55595c" />
+          <text x="50%" y="50%" fill="#eceeef" dy=".3em">
+            Thumbnail
+          </text>
+        </svg>
 
-const AlbumMain = () => {
-  return (
-    <div className="album py-5 bg-light">
-      <div className="container">
-        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-          <AlbumCard text="This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer." />
-          {/* Other AlbumCards */}
+        <div className="card-body">
+          <p className="card-text">{text}</p>
+          <div className="d-flex justify-content-between align-items-center">
+            <div className="btn-group">
+              <button
+                type="button"
+                className="btn btn-sm btn-outline-secondary"
+              >
+                사이트 들어가기
+              </button>
+            </div>
+            <small className="text-muted">9 mins</small>
+          </div>
         </div>
       </div>
     </div>
   );
 };
 
+const AlbumMain = () => {
+  return (
+    <main>
+      <section className="py-5 text-center container">
+        <div className="row py-lg-5">
+          <div className="col-lg-6 col-md-8 mx-auto">
+            <h1 className="fw-light">It Circle Alarm</h1>
+            <p className="lead text-muted">
+              IT 연합 동아리 정보를 여기서 확인하세요.
+            </p>
+          </div>
+        </div>
+      </section>
+
+      <div className="album py-5 bg-light">
+        <div className="container">
+          <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+            <AlbumCard text="This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer." />
+            {/* Repeat the AlbumCard for as many cards as you have */}
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
 const AlbumFooter = () => {
   // Add footer content
-  return <div></div>;
+  return (
+    <footer class="text-muted py-5">
+      <div class="container">
+        <p class="float-end mb-1">
+          <a href="#">Back to top</a>
+        </p>
+        <p class="mb-1"></p>
+      </div>
+    </footer>
+  );
 };
 
 function App() {
